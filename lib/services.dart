5e500122d38b17381e320dab3004dadc0aacfb0d
Andres Atehortua <andres.atehortua@zemoga.com>
2022-12-02 10:59:24 -0500

    Remove deprecated SharedPreferencesCache code.

--- lib/services.dart
+++ lib/services.dart
@@ -1,13 +1,7 @@
 library z.flutter.services;
 
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:z_dart/convert.dart';
-import 'package:z_dart/io.dart' show LegacyCache;
-
 export 'package:package_info_plus/package_info_plus.dart';
 export 'package:path_provider/path_provider.dart';
 export 'package:share_plus/share_plus.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:url_launcher/url_launcher.dart';
-
-part 'src/services/shared_preferences_cache.dart';

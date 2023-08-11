93b44fed975e0cd690b14489918f4eb5e3a2e323
Andres Atehortua <andres.atehortua@zemoga.com>
2023-08-11 11:12:47 -0500

    Remove proxy dependencies.

--- lib/services.dart
+++ lib/services.dart
@@ -1,7 +1,3 @@
 library z.flutter.services;
 
-export 'package:package_info_plus/package_info_plus.dart';
-export 'package:path_provider/path_provider.dart';
-export 'package:share_plus/share_plus.dart';
 export 'package:shared_preferences/shared_preferences.dart';
-export 'package:url_launcher/url_launcher.dart';

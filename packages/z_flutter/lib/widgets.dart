e9d5a79959c9e5a1b1dc151a8744d25956fadf66
Andres Atehortua <andres.atehortua@zemoga.com>
2023-09-14 10:45:01 -0500

    Remove not-useful widgets.

--- packages/z_flutter/lib/widgets.dart
+++ packages/z_flutter/lib/widgets.dart
@@ -1,17 +1,12 @@
 library z.flutter.widgets;
 
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart' show SvgPicture;
+import 'package:flutter/widgets.dart';
 import 'package:sliver_tools/sliver_tools.dart' show MultiSliver;
-
-import 'rendering.dart' show Dimens;
 
 export 'package:flutter/widgets.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:lottie/lottie.dart';
 export 'package:sliver_tools/sliver_tools.dart';
 
-part 'src/widgets/basic.dart';
 part 'src/widgets/media_query.dart';
 part 'src/widgets/scroll_view.dart';
-part 'src/widgets/sliver.dart';
